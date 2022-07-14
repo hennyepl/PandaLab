2b0aac4fed6b7d51c103634ad7aa52c0e60e5243
Henok Lamiso <epapalink68@gmail.com>
2022-07-14 17:42:18 -0600

    main menu v1.2

--- .github/workflows/src/main.cpp
+++ .github/workflows/src/main.cpp
@@ -4,23 +4,30 @@
 /* AUTHOR: HENOK LAMISO                                                       */
 /* DATE: JULY 2022                                                            */
 /* VERSION: 1.0                                                               */
+/* CONTACT: https://www.henoktamirat.com                                      */
 /* DESCRIPTION: THIS FILE CONTAINES THE HOME PAGE FOR THE PROGRAM             */
 /******************************************************************************/
 
 #include <iostream>
-
+#include <iomanip>
 int main ()
 {
-    // Main menu
     // centerd on the full with screen with a border
     // background color is black and text color is Yellow
     // the menu is displayed in the center of the screen
     
     std::cout << "\033[1;33m" << "\033[1;47m" << "\n";
+    // apply diffrent font to the text 
+    
 
 
+    // set text in the center of the screen using the setw function
     std::cout << "*************************************************************" << std::endl;
-    std::cout << "*  WELLCOME                                                 *" << std::endl;
+    std::cout << "*"<< std::setw(30) << "WELLCOME" << std::setw(30) << "*" << std::endl;
+    std::cout << std::setfill('*') << std::setw(61) << "*" << std::endl;
+
+    
+    std::cout << "\n" << std::setfill('*') << std::setw(61) << "*" << std::endl;
     std::cout << "*                                                           *" << std::endl;
     std::cout << "*                                                           *" << std::endl;
     std::cout << "*************************************************************" << std::endl;
