91813a0986f119c3d403ec155f0e038903257dc7
Henok Lamiso <epapalink68@gmail.com>
2022-07-14 16:24:14 -0600

    CI error fixed

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ int main ()
     // the menu is displayed in the center of the screen
     
     std::cout << "\033[1;33m" << "\033[1;47m" << "\n";
+
+
     std::cout << "*************************************************************" << std::endl;
     std::cout << "*  WELLCOME                                                 *" << std::endl;
     std::cout << "*                                                           *" << std::endl;
