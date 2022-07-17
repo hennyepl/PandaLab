7084676981071e94c07f70afbf768ed393edea3f
Henok Lamiso <epapalink68@gmail.com>
2022-07-16 21:32:11 -0600

    linearSearch implemanted

--- src/algothims/LinearSearch/linearSearch.cpp
+++ src/algothims/LinearSearch/linearSearch.cpp
@@ -1,6 +1,64 @@
 #include "linearSearch.h"
+#include <iostream>
 
+// call linearSearch function to search for a value in an array from linearSearch.h
 int main()
 {
+    // ask user for the size of the array
+    int size;
+    std::cout << "Enter the size of the array: ";
+    std::cin >> size;
+
+    // ask user to enter the values of the array size = size
+    int array[size];
+    std::cout << "Enter the values of the array: "; 
+    for (int i = 0; i < size; i++)
+    {
+        std::cin >> array[i];
+    }
+
+    // ask user to enter the value to be searched
+    int value;
+    std::cout << "Enter the value to be searched: ";
+    std::cin >> value;
+
+    // call linearSearch function to search for a value in an array, if the value is not found on the first
+    // search, ask the user if they want to search again and again until the value is found, or until the user
+    // say no to the search.
+    bool found = linearSearch(array, value, size);
+    if (found == false)
+    {
+        std::cout << "The value is not found in the array.\n";
+        std::cout << "Do you want to search again? (y/n): ";
+        char answer;
+        std::cin >> answer;
+        while (answer == 'y')
+        {
+            std::cout << "Enter the value to be searched: ";
+            std::cin >> value;
+            found = linearSearch(array, value, size);
+            if (found == true)
+            {
+                std::cout << "The value is found in the array.\n";
+                break;
+            }
+            else
+            {
+                std::cout << "The value is not found in the array.\n";
+                std::cout << "Do you want to search again? (y/n): ";
+                std::cin >> answer;
+                // print the following message with red color
+                std::cout << "\033[1;31m"; 
+                std::cout << "\n**\tSEARCH TERMINATED\t**\n";
+                std::cout << "\033[0m" << "\n**\tGoodbye for now!\t**\n\n";
+            }
+        }
+    }
+    else
+    {
+        std::cout << "The value is found in the array.\n";
+    }
+
+
     return 0;
 }
--- src/algothims/LinearSearch/linearSearch.h
+++ src/algothims/LinearSearch/linearSearch.h
@@ -1,5 +1,47 @@
+// This header file contains the implementation of the linear search algorithm.
+// The implementation is based on the following algorithm:
+// 1. Declare a function named linearSearch that takes two parameters:
+//    a. An array of integers   (int array[])
+//    b. The value to be searched (int value)
+// 2. Declare a variable named index that will hold the index of the value to be searched.
+// 3. Declare a variable named found that will hold the value of true or false.
+// 4. Declare a variable named i that will hold the index of the array.
+// 5. Declare a variable named j that will hold the index of the array. (j = 0)
+// 6. Declare a variable named found that will hold the value of true or false. (found = false) 
+// finally, return the value of found.
+
+
 #ifndef LINEAR_SEARCH_H
 #define LINEAR_SEARCH_H
+
+// class that contains the function to search for a value in an array but not the implementation of the function.
+class LinearSearch
+{
+public:
+    // function that searches for a value in an array.
+    bool linearSearch(int array[], int value, int size);
+};
+
+// implementation of the function.
+bool linearSearch(int array[], int value, int size)
+{
+    int index = 0;
+    bool found = false;
+    int i = 0;
+    int j = 0;
+    while (i < size)
+    {
+        if (array[i] == value)
+        {
+            found = true;
+            index = i;
+            break;
+        }
+        i++;
+    }
+    return found;
+}
+
 
 
 
